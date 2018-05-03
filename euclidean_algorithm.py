267007ed2803267fb28c2ebc5f662cd159baed5a
Yimei Huang <yhuang@akamai.com>
2018-05-02 22:40:58 -0400

    Add another test case

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,4 +1,7 @@
 def euclidean(a, b):
+    '''
+    find x, y such that ax+by=1
+    '''
     q = a // b
     r = a % b
 
@@ -17,9 +20,14 @@ print('verify result', verify_euc(314, 159, 1, e1))
 # there is other solution (-40, 79) 
 print('verify result', verify_euc(314, 159, 1, (-40, 79))) 
 
+e2 = euclidean(4144, 7696)
+print('find (x, y) such that 4144x + 7696y = 592, result=', e2)
+
+print('verify result e2', verify_euc(4144, 7696, 592, e2))
 '''
 python3 euclidean_algorithm.py 
 find (x, y) such that 314x + 159y = 1, result= (119, -235)
 verify result True
 verify result True
 '''
+
